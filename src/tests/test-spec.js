67840b4f1b0c0ed8f098d3ac92af00340437d8d3
Pablo_Rufat_Munoz <pablo.munoz.ext@talentfour.com.br>
2021-03-01 19:34:21 -0300

    first commit

---
+++ src/tests/test-spec.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var chai_1 = require("chai");
+it('should fail', function () {
+    chai_1.expect(true).to.equal(false);
+});
---
+++ src/tests/test-spec.ts
@@ -0,0 +1,5 @@
+import { expect } from 'chai';
+
+it('should fail', () => {
+    expect(true).to.equal(false);
+});
